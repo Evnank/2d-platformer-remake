b8b0f5ed433cb9f35c51316a638f32dfb459e775
Evnank <1esh4zhuk@gmail.com>
2026-05-25 22:19:27 +0300

    cmake and gitignore is ready

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,34 @@
 #include <SFML/Graphics.hpp>
+class GAME{
+private:
+	
+public:
+	void RUN();
+	
+};
+void GAME::RUN(){
+		sf::RenderWindow window( sf::VideoMode( { 200, 200 } ), "SFML works!" );
+		sf::CircleShape shape( 100.f );
+		shape.setFillColor( sf::Color::Green );
+
+		while ( window.isOpen() )
+		{
+			while ( const std::optional event = window.pollEvent() )
+			{
+				if ( event->is<sf::Event::Closed>() )
+					window.close();
+			}
+
+			window.clear();
+			window.draw( shape );
+			window.display();
+		}
+	}
+
+
 
 int main()
 {
-	sf::RenderWindow window( sf::VideoMode( { 200, 200 } ), "SFML works!" );
-	sf::CircleShape shape( 100.f );
-	shape.setFillColor( sf::Color::Green );
-
-	while ( window.isOpen() )
-	{
-		while ( const std::optional event = window.pollEvent() )
-		{
-			if ( event->is<sf::Event::Closed>() )
-				window.close();
-		}
-
-		window.clear();
-		window.draw( shape );
-		window.display();
-	}
+	GAME game;
+	game.RUN();
 }
