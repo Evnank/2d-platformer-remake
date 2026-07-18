e2c26fb1c9fd3f116ce571c3173b7fb5805f6579
Evnank <1esh4zhuk@gmail.com>
2026-07-18 23:08:05 +0300

    fixed editor bugs

--- src/CTRL_Z.cpp
+++ src/CTRL_Z.cpp
@@ -23,12 +23,12 @@ void CTRL_Z_ACTION::GO_BACKWARDS(std::vector<ENTITY>& entities,std::unordered_ma
     }
     case CTRL_Z_ACTION_TYPE::DELETE_ENTITTY:
     {
-        entities.push_back(old_entity);
+        entities.insert(entities.begin()+index_of_entity_changed,old_entity);
         break;
     }
      case CTRL_Z_ACTION_TYPE::PLACE_ENTITY:
     {
-        entities.pop_back();
+         entities.erase(entities.begin()+index_of_entity_changed);
         request_to_select_entity=true;
         break;
     }
@@ -61,16 +61,51 @@ void CTRL_Z_ACTION::GO_FORWARD(std::vector<ENTITY>& entities,std::unordered_map<
     }
     case CTRL_Z_ACTION_TYPE::DELETE_ENTITTY:
     {
-        entities.push_back(old_entity);
+        
+        entities.erase(entities.begin()+index_of_entity_changed);
         request_to_select_entity=true;
         break;
     }
      case CTRL_Z_ACTION_TYPE::PLACE_ENTITY:
     {
-        entities.pop_back();
+        entities.insert(entities.begin()+index_of_entity_changed,old_entity); 
         break;
     }
     default:
         break;
     }
 }
+
+
+
+
+void CTRL_Z::UPDATE(INPUT& input,std::vector<ENTITY>& entities,std::unordered_map<std::pair<int,int>,GAME_CHUNK,PairHash>& game_chunks){
+        if (current_ctrl_z_action.type != CTRL_Z_ACTION_TYPE::NOTHING){
+            std::cout<<"1:  index="<<current_action_index<<"; size="<<actions.size()<<"\n";
+            if (actions.size()!=0){
+                actions.erase(actions.begin()+current_action_index+1,actions.end());
+                std::cout<<"ALO:  index="<<current_action_index<<"; size="<<actions.size()<<"\n";
+            }
+            actions.push_back(current_ctrl_z_action);
+            std::cout<<"3:  index="<<current_action_index<<"; size="<<actions.size()<<"\n";
+            current_action_index=actions.size()-1;
+            std::cout<<"4:  index="<<current_action_index<<"; size="<<actions.size()<<"\n\n";
+            current_ctrl_z_action.type=CTRL_Z_ACTION_TYPE::NOTHING;
+        } 
+        UPDATE_INPUT(input,entities,game_chunks);
+    }
+
+    void CTRL_Z::UPDATE_INPUT(INPUT& input,std::vector<ENTITY>& entities,std::unordered_map<std::pair<int,int>,GAME_CHUNK,PairHash>& game_chunks){
+        int a=current_action_index;
+        int b=actions.size();
+        std::cout<<"TEST:  index="<<a<<"; size="<<b<<"\n";
+        if (actions.size()!=0 && sf::Keyboard::isKeyPressed(sf::Keyboard::Key::LControl)){
+            if (input.Z && current_action_index>=0){
+                actions[current_action_index].GO_BACKWARDS(entities,game_chunks,request_to_select_entity);
+                current_action_index--;
+            } else if (input.Y && a<b-1){
+                actions[current_action_index+1].GO_FORWARD(entities,game_chunks,request_to_select_entity);
+                current_action_index++;
+            }
+        }
+    }
--- src/CTRL_Z.h
+++ src/CTRL_Z.h
@@ -36,28 +36,11 @@ struct CTRL_Z{
     int current_action_index=-1;
     bool request_to_select_entity=false;
 
-    void UPDATE(INPUT& input,std::vector<ENTITY>& entities,std::unordered_map<std::pair<int,int>,GAME_CHUNK,PairHash>& game_chunks){
-        if (current_ctrl_z_action.type!=CTRL_Z_ACTION_TYPE::NOTHING){
-            if (current_action_index<=actions.size()-1 && actions.size()!=0){
-                actions.erase(actions.begin()+current_action_index+1,actions.end());
-                std::cout<<"erased, size: "<<actions.size()<<"; ind: "<<current_action_index<<"\n";
-            }
-            actions.push_back(current_ctrl_z_action);
-            current_action_index=actions.size()-1;
-            current_ctrl_z_action.type=CTRL_Z_ACTION_TYPE::NOTHING;
-        } 
-        if (actions.size()!=0 && sf::Keyboard::isKeyPressed(sf::Keyboard::Key::LControl)){
-            if (input.Z && current_action_index>=0){
-                actions[current_action_index].GO_BACKWARDS(entities,game_chunks,request_to_select_entity);
-                current_action_index--;
-            } else if (input.Y && current_action_index<actions.size()-1){
-                actions[current_action_index+1].GO_FORWARD(entities,game_chunks,request_to_select_entity);
-                current_action_index++;
-            }
-        }
-    }
+    void UPDATE(INPUT& input,std::vector<ENTITY>& entities,std::unordered_map<std::pair<int,int>,GAME_CHUNK,PairHash>& game_chunks);
+
+    void UPDATE_INPUT(INPUT& input,std::vector<ENTITY>& entities,std::unordered_map<std::pair<int,int>,GAME_CHUNK,PairHash>& game_chunks);
 
     void DRAW(){
-
+        
     }
 };
